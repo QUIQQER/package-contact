c0b7663087a06b694e54ef9d1774dc246a323467
Henning Leutz <leutz@pcsg.de>
2015-09-16 18:00:48 +0200

    formbuilder intecration

--- types/contact.php
+++ types/contact.php
@@ -1,6 +1,13 @@
 <?php
 
+$formData = json_decode($Site->getAttribute('quiqqer.contact.settings.form'), true);
+
+if (!is_array($formData)) {
+    $formData = array();
+}
+
 $Form = new QUI\FormBuilder\Builder();
+$Form->load($formData);
 
 $Engine->assign(array(
     'Form' => $Form
