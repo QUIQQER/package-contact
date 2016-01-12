8fc27dd2d0fc91c9adfe22a43f09cc2704e1a9cf
Henning Leutz <leutz@pcsg.de>
2016-01-12 15:27:01 +0100

    hklused/website#63 - cache problem solved -> site to no cache if post is not empty

---
+++ src/QUI/Contact/EventHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file contains QUI\Contact\EventHandler
+ */
+namespace QUI\Contact;
+
+/**
+ * Class EventHandler
+ *
+ * @package QUI\Contact
+ */
+class EventHandler
+{
+    /**
+     * @param \QUI\Projects\Site $Site
+     */
+    public static function onSiteInit($Site)
+    {
+        if ($Site->getAttribute('type') == 'quiqqer/contact'
+            && !empty($_POST)
+        ) {
+            $Site->setAttribute('nocache', 1);
+        }
+    }
+}
